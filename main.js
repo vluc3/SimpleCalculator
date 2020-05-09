14642a82ef51a76871b3d5361020cd86c3da6656
Vincent LUC <vluc3@hotmail.fr>
2020-05-09 12:59:12 +0200

    Correct compilation error on updateResult() function

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function clearResult() {
 }
 
 function updateResult(result) {
-  let element = document.getElementBy('result');
+  let element = document.getElementById('result');
 
   if (element) {
     element.innerText = result;
